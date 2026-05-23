0ef02274ea3f4670ac44ab4c91479d56382f4f78
Ubuntu <ubuntu@fv4bluq9ce0lml57k72t.auto.internal>
2026-05-23 18:31:33 +0000

    udalil probeli

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,60 +9,60 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost,158.160.224.159,backend,gateway,kittygram12.ydns.eu').split(',')
 
-INSTALLED_APPS = [ 
-    'django.contrib.admin', 
-    'django.contrib.auth', 
-    'django.contrib.contenttypes', 
-    'django.contrib.sessions', 
-    'django.contrib.messages', 
-    'django.contrib.staticfiles', 
-    'rest_framework.authtoken', 
-    'rest_framework', 
-    'djoser', 
-    'cats.apps.CatsConfig', 
-] 
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework.authtoken',
+    'rest_framework',
+    'djoser',
+    'cats.apps.CatsConfig',
+]
 
-MIDDLEWARE = [ 
-    'django.middleware.security.SecurityMiddleware', 
-    'django.contrib.sessions.middleware.SessionMiddleware', 
-    'django.middleware.common.CommonMiddleware', 
-    'django.middleware.csrf.CsrfViewMiddleware', 
-    'django.contrib.auth.middleware.AuthenticationMiddleware', 
-    'django.contrib.messages.middleware.MessageMiddleware', 
-    'django.middleware.clickjacking.XFrameOptionsMiddleware', 
-] 
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
-ROOT_URLCONF = 'kittygram_backend.urls' 
+ROOT_URLCONF = 'kittygram_backend.urls'
 
-TEMPLATES = [ 
-    { 
-        'BACKEND': 'django.template.backends.django.DjangoTemplates', 
-        'DIRS': [], 
-        'APP_DIRS': True, 
-        'OPTIONS': { 
-            'context_processors': [ 
-                'django.template.context_processors.debug', 
-                'django.template.context_processors.request', 
-                'django.contrib.auth.context_processors.auth', 
-                'django.contrib.messages.context_processors.messages', 
-            ], 
-        }, 
-    }, 
-] 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
-WSGI_APPLICATION = 'kittygram_backend.wsgi.application' 
+WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 
 if os.getenv('DB_ENGINE') == 'django.db.backends.postgresql':
-    DATABASES = { 
-        'default': { 
-            'ENGINE': os.getenv('DB_ENGINE'), 
-            'NAME': os.getenv('POSTGRES_DB', 'postgres'), 
-            'USER': os.getenv('POSTGRES_USER', 'postgres'), 
-            'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'), 
-            'HOST': os.getenv('DB_HOST', 'db'), 
-            'PORT': os.getenv('DB_PORT', '5432'), 
-        } 
+    DATABASES = {
+        'default': {
+            'ENGINE': os.getenv('DB_ENGINE'),
+            'NAME': os.getenv('POSTGRES_DB', 'postgres'),
+            'USER': os.getenv('POSTGRES_USER', 'postgres'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+            'HOST': os.getenv('DB_HOST', 'db'),
+            'PORT': os.getenv('DB_PORT', '5432'),
+        }
     }
 else:
     DATABASES = {
@@ -73,49 +73,49 @@ else:
     }
 
 
-AUTH_PASSWORD_VALIDATORS = [ 
-    { 
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', 
-    }, 
-    { 
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', 
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
 
-    }, 
-    { 
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', 
-    }, 
-    { 
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', 
-    }, 
-] 
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 
-LANGUAGE_CODE = 'ru-ru' 
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'Europe/Moscow' 
+TIME_ZONE = 'Europe/Moscow'
 
-USE_I18N = True 
+USE_I18N = True
 
-USE_L10N = True 
+USE_L10N = True
 
-USE_TZ = True 
+USE_TZ = True
 
 
-STATIC_URL = '/static/' 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
-MEDIA_URL = '/media/' 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField' 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = { 
-    'DEFAULT_PERMISSION_CLASSES': [ 
-        'rest_framework.permissions.IsAuthenticated',  
-    ], 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 
-    'DEFAULT_AUTHENTICATION_CLASSES': [ 
-        'rest_framework.authentication.TokenAuthentication', 
-    ], 
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 
-    'PAGE_SIZE': 10, 
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
