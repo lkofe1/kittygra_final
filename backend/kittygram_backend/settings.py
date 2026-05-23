6954915e81b59f64cdf87c3fcb5c194498fdd857
lkofe1 <denisburan5@gmail.com>
2026-05-23 20:49:16 +0400

    1222

---
+++ backend/kittygram_backend/settings.py
@@ -0,0 +1,123 @@
+# flake8: noqa 
+
+import os 
+from pathlib import Path 
+
+BASE_DIR = Path(__file__).resolve().parent.parent 
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'default-key') 
+
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost,158.160.224.159,backend,gateway').split(',')
+
+INSTALLED_APPS = [ 
+    'django.contrib.admin', 
+    'django.contrib.auth', 
+    'django.contrib.contenttypes', 
+    'django.contrib.sessions', 
+    'django.contrib.messages', 
+    'django.contrib.staticfiles', 
+    'rest_framework.authtoken', 
+    'rest_framework', 
+    'djoser', 
+    'cats.apps.CatsConfig', 
+] 
+
+MIDDLEWARE = [ 
+    'django.middleware.security.SecurityMiddleware', 
+    'django.contrib.sessions.middleware.SessionMiddleware', 
+    'django.middleware.common.CommonMiddleware', 
+    'django.middleware.csrf.CsrfViewMiddleware', 
+    'django.contrib.auth.middleware.AuthenticationMiddleware', 
+    'django.contrib.messages.middleware.MessageMiddleware', 
+    'django.middleware.clickjacking.XFrameOptionsMiddleware', 
+] 
+
+ROOT_URLCONF = 'kittygram_backend.urls' 
+
+TEMPLATES = [ 
+    { 
+        'BACKEND': 'django.template.backends.django.DjangoTemplates', 
+        'DIRS': [], 
+        'APP_DIRS': True, 
+        'OPTIONS': { 
+            'context_processors': [ 
+                'django.template.context_processors.debug', 
+                'django.template.context_processors.request', 
+                'django.contrib.auth.context_processors.auth', 
+                'django.contrib.messages.context_processors.messages', 
+            ], 
+        }, 
+    }, 
+] 
+
+WSGI_APPLICATION = 'kittygram_backend.wsgi.application' 
+
+
+if os.getenv('DB_ENGINE') == 'django.db.backends.postgresql':
+    DATABASES = { 
+        'default': { 
+            'ENGINE': os.getenv('DB_ENGINE'), 
+            'NAME': os.getenv('POSTGRES_DB', 'postgres'), 
+            'USER': os.getenv('POSTGRES_USER', 'postgres'), 
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'), 
+            'HOST': os.getenv('DB_HOST', 'db'), 
+            'PORT': os.getenv('DB_PORT', '5432'), 
+        } 
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+
+AUTH_PASSWORD_VALIDATORS = [ 
+    { 
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', 
+    }, 
+    { 
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', 
+
+    }, 
+    { 
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', 
+    }, 
+    { 
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', 
+    }, 
+] 
+
+LANGUAGE_CODE = 'ru-ru' 
+
+TIME_ZONE = 'Europe/Moscow' 
+
+USE_I18N = True 
+
+USE_L10N = True 
+
+USE_TZ = True 
+
+
+STATIC_URL = '/static/' 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
+MEDIA_URL = '/media/' 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField' 
+
+REST_FRAMEWORK = { 
+    'DEFAULT_PERMISSION_CLASSES': [ 
+        'rest_framework.permissions.IsAuthenticated',  
+    ], 
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [ 
+        'rest_framework.authentication.TokenAuthentication', 
+    ], 
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 
+    'PAGE_SIZE': 10, 
+}
